9fb6e92eda00a54965740b5bf90ff0bd2d56d925
Amina-farah <aminaali5580@gmail.com>
2023-01-17 05:20:10 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,18 +1,23 @@
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
 
 /**
- * free_dog - free memory of dog
- * @d: pointer to dog
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
  */
-void free_dog(dog_t *d)
+int _strlen(char *s)
 {
-	if (d)
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
 	{
-		if (d->name)
-			free(d->name);
-		if (d->owner)
-			free(d->owner);
-		free(d);
+		i++;
 	}
+
+	return (i);
 }
+
